5359372ecee2fcaca19bff8b8471332be7b4b6c5
INCHUL <53209052+IM-INCHER@users.noreply.github.com>
2023-04-30 20:13:58 +0900

    \t 수정

--- MakeScanf/MakeScanf/MakeScanf.cpp
+++ MakeScanf/MakeScanf/MakeScanf.cpp
@@ -10,22 +10,20 @@ void MyScanf(const char* format, void* a1, void* a2, void* a3, void* a4);
 
 int main()
 {
-
     int a = 0;
     float b = 0;
     char c = 0;
-    char d[100];
+    char* d = NULL;
 
-    MyScanf("%d %f %c %s", &a, &b, &c, &d);
+    MyScanf("%d\t%f\t%c\t%s", &a, &b, &c, &d);
 
     printf("%d, %f, %c, %s\n", a, b, c, d);
-
     return 0;
 }
 
 void MyScanf(const char* format, void* a1, void* a2, void* a3, void* a4)
 {
-    char input[100];  
+    char input[1024];  
     char* token;      
     int index = 0;
 
@@ -37,7 +35,7 @@ void MyScanf(const char* format, void* a1, void* a2, void* a3, void* a4)
     gets_s(input, sizeof(input));
 
     // 입력받은 문자열을 지정된 형식에 따라 값을 저장
-    token = strtok_s(input, " ", &context);
+    token = strtok_s(input, " \t", &context);
     while (*format) {
         if (*format == '%') {
             switch (*(++format)) {
@@ -45,28 +43,28 @@ void MyScanf(const char* format, void* a1, void* a2, void* a3, void* a4)
                 if (token != NULL) {
                     *(int*)var[index] = atoi(token);
                     index++;
-                    token = strtok_s(NULL, " ", &context);
+                    token = strtok_s(NULL, " \t", &context);
                 }
                 break;
             case 'f':
                 if (token != NULL) {
                     *(float*)var[index] = atof(token);
                     index++;
-                    token = strtok_s(NULL, " ", &context);
+                    token = strtok_s(NULL, " \t", &context);
                 }
                 break;
             case 'c':
                 if (token != NULL) {
                     *(char*)var[index] = token[0];
                     index++;
-                    token = strtok_s(NULL, " ", &context);
+                    token = strtok_s(NULL, " \t", &context);
                 }
                 break;
             case 's':
                 if (token != NULL) {
                     *(char**)var[index] = token;
                     index++;
-                    token = strtok_s(NULL, " ", &context);
+                    token = strtok_s(NULL, " \t", &context);
                 }
                 break;
             default:
