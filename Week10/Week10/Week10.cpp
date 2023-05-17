a014ef18c347c9d2ff41f038a114709310ce8b60
INCHUL <53209052+IM-INCHER@users.noreply.github.com>
2023-05-18 05:39:41 +0900

    과제

--- Week10/Week10/Week10.cpp
+++ Week10/Week10/Week10.cpp
@@ -1,9 +1,8 @@
 ﻿#define BUFMAX 1000
 
 #include <stdio.h>
-#include <string>
-
-using namespace std;
+#include <string.h>
+#include <stdlib.h>
 
 struct Box
 {
@@ -13,37 +12,40 @@ struct Box
 
     int iArr = 0;
     char cArr = 0;
-    char* sArr = NULL;
+    char sArr[100] = "";
 };
 
 struct Node {
     char* value;
-    Node* child;
-    Node* sibling;
+    Node* child = nullptr;
+    Node* sibling = nullptr;
 };
 
 Node* createNode(const char* value);
-void insertChild(Node* parent, Node* child); 
+void insertChild(Node* parent, Node* child);
 void parseString(Node* node, Box* b);
-void destroyTree(Node* node); 
+void parsingString(Node* root, char* value, Box* b);
+void destroyTree(Node* node);
 
-int main() 
+int main()
 {
     FILE* fp;
     fopen_s(&fp, "a.txt", "r");
 
     Box box;
 
-    char buffer[BUFMAX] = { 0, };
+    char buffer[BUFMAX] = { 0 };
 
-    fread(buffer, BUFMAX, 1, fp);
-
-    printf("%s\n", buffer);
+    if (fgets(buffer, BUFMAX, fp) == nullptr)
+    {
+        printf("문장을 읽어오지 못했습니다.\n");
+        return 1;
+    }
 
     Node* root = nullptr;
     Node* currentNode = nullptr;
 
-    char* context = NULL;
+    char* context = nullptr;
 
     char* token = strtok_s(buffer, " ", &context);
     while (token != nullptr) {
@@ -59,11 +61,39 @@ int main()
         token = strtok_s(nullptr, " ", &context);
     }
 
-    parseString(root,&box);
+    parseString(root, &box);
 
-    printf("int = %d\nchar = %d\nchar* = %d\n", box.i, box.c, box.s);
+    //box.iArr = (int*)malloc(sizeof(int) * box.i);
+    //box.cArr = (char*)malloc(sizeof(char) * box.c);
+    //box.sArr = (char**)malloc(sizeof(char*) * box.s);
+
+    if (fgets(buffer, BUFMAX, fp) == nullptr)
+    {
+        printf("문장을 읽어오지 못했습니다.\n");
+        return 1;
+    }
+
+    token = NULL;
+    context = NULL;
+
+    token = strtok_s(buffer, " ", &context);
+    while (token != nullptr) {
+        parsingString(root ,token, &box);
+
+        token = strtok_s(nullptr, " ", &context);
+    }
+
+    printf("int(%d): %d\n", box.i, box.iArr);
+    printf("char(%d): %c\n", box.c, box.cArr);
+    printf("char*(%d): %s\n", box.s, box.sArr);
 
     destroyTree(root);
+
+    fclose(fp);
+
+    /*free(box.iArr);
+    free(box.cArr);
+    free(box.sArr);*/
 
     return 0;
 }
@@ -84,11 +114,11 @@ void insertChild(Node* parent, Node* child)
         parent->child = child;
     }
     else {
-        Node* sibling = parent->child;
+        /*Node* sibling = parent->child;
         while (sibling->sibling != nullptr) {
             sibling = sibling->sibling;
         }
-        sibling->sibling = child;
+        sibling->sibling = child;*/
     }
 }
 
@@ -99,7 +129,7 @@ void parseString(Node* node, Box* b)
     }
 
     char* value = node->value;
-    if (value[0] == '%' && value[1] != '\0') 
+    if (value[0] == '%' && value[1] != '\0')
     {
         char specifier = value[1];
         switch (specifier) {
@@ -116,10 +146,56 @@ void parseString(Node* node, Box* b)
             printf("Unknown specifier: %c", specifier);
             break;
         }
+        char str[3] = { value[0] , value[1] };
+        Node* newNode = createNode(str);
+        node->sibling = newNode;
+
+        char* str2 = value + 2;
+        newNode = createNode(str2);
+        node->sibling->child = newNode;
     }
 
     parseString(node->child, b);
-    parseString(node->sibling, b);
+    //parseString(node->sibling, b);
+}
+
+void parsingString(Node* node, char* value, Box* b)
+{
+    if (node->child == NULL) return;
+    if (node->sibling == NULL)
+    {
+        parsingString(node->child, value, b);
+        return;
+    }
+
+    char* str = strstr(value, node->sibling->child->value);
+
+    if (str == NULL)
+    {
+        parsingString(node->child, value, b);
+        return;
+    }
+
+    int len = strlen(str);
+    char str2[100] = "";
+    strncat_s(str2, sizeof(str2), value, strlen(value) - len);
+
+    switch (node->sibling->value[1])
+    {
+    case 'd':
+        b->iArr = atoi(str2);
+        break;
+    case 'c':
+        b->cArr = str2[0];
+        break;
+    case 's':
+        //b->sArr = str2;
+
+        strncat_s(b->sArr, sizeof(b->sArr), str2, strlen(str2));
+        break;
+    default:
+        break;
+    }
 }
 
 void destroyTree(Node* node)
